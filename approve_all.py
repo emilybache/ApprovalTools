3d29f16fc605f508ea4b8f8ec6bbdcef2a4d5977
Emily Bache <emily.bache@praqma.com>
2019-12-12 13:18:19 +0100

    upgrade to use argparse and provide debug messages while it runs

--- approve_all.py
+++ approve_all.py
@@ -1,23 +1,38 @@
 import os
 import re
 import shutil
+import argparse
 
 
-def approve_all(directory):
+def approve_all(directory, verbose):
     regex = re.compile("(.*\.)received(\..*)")
     for root, dirs, files in os.walk(directory):
         for filename in files:
             matches = re.findall(regex, filename)
             if matches:
                 new_filename = matches[0][0] + "approved" + matches[0][1]
-                shutil.copyfile(str(os.path.join(root, filename)),
-                                str(os.path.join(root, new_filename)))
+                received_file = str(os.path.join(root, filename))
+                approved_file = str(os.path.join(root, new_filename))
+                shutil.copyfile(received_file,
+                                approved_file)
+                if verbose:
+                    print(f"approving {matches[0][0]}")
 
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) > 1:
-        directory = sys.argv[1]
-    else:
-        directory = os.getcwd()
-    approve_all(directory)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory", help="the directory to approve files in")
+    parser.add_argument("-q", "--quiet", action="store_true", help="suppress all output messages")
+
+    args = parser.parse_args()
+    directory = args.directory or os.getcwd()
+    verbose = not args.quiet
+
+    if not os.path.exists(directory):
+        print(f"directory not found {directory}")
+        sys.exit(-1)
+
+    if verbose:
+        print(f"approving all received files in folder {directory}")
+    approve_all(directory, verbose)
