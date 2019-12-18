39b108dfaa4c49e9174b3626ab70d13e982306a8
Emily Bache <emily.bache@praqma.com>
2019-12-18 14:14:43 +0100

    added script to clean up received files

--- clean_received.py
+++ clean_received.py
@@ -1,28 +1,24 @@
+import argparse
 import os
 import re
-import shutil
-import argparse
 
 
-def approve_all(directory, verbose=True):
-    regex = re.compile("(.*\.)received(\..*)")
+def clean_received(directory, verbose=True):
+    regex = re.compile("(.*)\.received\.(.*)")
     for root, dirs, files in os.walk(directory):
         for filename in files:
             matches = re.findall(regex, filename)
             if matches:
-                new_filename = matches[0][0] + "approved" + matches[0][1]
                 received_file = str(os.path.join(root, filename))
-                approved_file = str(os.path.join(root, new_filename))
-                shutil.copyfile(received_file,
-                                approved_file)
+                os.remove(received_file)
                 if verbose:
-                    print(f"approving {matches[0][0]}")
+                    print(f"removed {matches[0][0]}.received.{matches[0][1]}")
 
 
 if __name__ == "__main__":
     import sys
     parser = argparse.ArgumentParser()
-    parser.add_argument("directory", help="the directory to approve files in")
+    parser.add_argument("directory", help="the directory to clean files in")
     parser.add_argument("-q", "--quiet", action="store_true", help="suppress all output messages")
 
     args = parser.parse_args()
@@ -34,5 +30,5 @@ if __name__ == "__main__":
         sys.exit(-1)
 
     if verbose:
-        print(f"approving all received files in folder {directory}")
-    approve_all(directory, verbose)
+        print(f"cleaning received files in folder {directory}")
+    clean_received(directory, verbose)
---
+++ tests/test_clean_received.py
@@ -0,0 +1,29 @@
+from approvaltests import verify
+import os
+
+from clean_received import clean_received
+
+
+def test_empty_dir(tmpdir):
+    clean_received(tmpdir)
+    verify(str(os.listdir(tmpdir)))
+
+
+def test_simple_received_file(tmpdir):
+    received_file = os.path.join(tmpdir, "a.received.txt")
+    with open(received_file, "w") as f:
+        f.write("foo")
+    clean_received(tmpdir)
+    verify(str(os.listdir(tmpdir)))
+
+
+def test_mix_of_files(tmpdir):
+    received_file = os.path.join(tmpdir, "a.received.txt")
+    with open(received_file, "w") as f:
+        f.write("foo")
+    approved_file = os.path.join(tmpdir, "a.approved.txt")
+    with open(approved_file, "w") as f:
+        f.write("bar")
+    clean_received(tmpdir)
+    verify(str(os.listdir(tmpdir)))
+
